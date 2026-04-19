93a10d6fb2b2b5f7ddf9d3d16788b061d4ad7b7b
ferolero <ferolero@gmail.com>
2026-04-19 03:52:12 -0400

    added server logic

---
+++ server.py
@@ -0,0 +1,22 @@
+from EmotionDetection.emotion_detection import emotion_detector
+from flask import Flask, render_template, request 
+
+app = Flask("Emotion detector")
+
+@app.route("/")
+def render_index_page():
+    return render_template("index.html")
+
+@app.route("/emotionDetector")
+def detect_emotion():
+    text_to_analyze = request.args.get('textToAnalyze')
+    detector_response = emotion_detector(text_to_analyze=text_to_analyze)
+    return (f"For the given statement, the system response is "
+            f"\'anger\': {detector_response['anger']}, "
+            f"\'disgust\': {detector_response['disgust']}, "
+            f"\'fear\': {detector_response['fear']}, "
+            f"\'joy\': {detector_response['joy']} and "
+            f"\'sadness\': {detector_response['sadness']}. "
+            f"The dominant emotion is <b> {detector_response['dominant_emotion']}. </b>"
+            )
+
